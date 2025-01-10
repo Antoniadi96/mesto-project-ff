befe632ea313e3066b2036081ac74c57fcb116f2
Aleksandr Antoniadi <antoniadisasha@yandex.ru>
2025-01-11 00:57:40 +0500

    1.1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Проектная работа Mesto
+# Проектная работа Mesto, 5 спринт
+Вёрстка [макета](https://www.figma.com/design/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0-1&t=G5ebEnIvC38h6igw-1) "Mesto" по курсу Яндекс Практикума
 
+## Описание
+Задача на этом этапе — создавать и удалять карточки с помощью JavaScript. 
+
+### Ссылка на проект
+https://github.com/Antoniadi96/zakrivayuschiy-teg-f.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,42 @@
+import { initialCards } from './cards.js';
+
 // @todo: Темплейт карточки
+
+const cardTemplate = document.getElementById('card-template').content;
 
 // @todo: DOM узлы
 
+const placesList = document.querySelector('.places__list');
+
 // @todo: Функция создания карточки
+
+function createCard(initialCard, deleteCard) {
+    const card = cardTemplate.cloneNode(true);
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+
+    cardImage.src = initialCard.link;
+    cardImage.alt = initialCard.name;
+    cardTitle.textContent = initialCard.name;
+
+    cardDeleteButton.addEventListener('click', function() {
+        const cardToDelete = cardDeleteButton.closest('.places__item');
+        deleteCard(cardToDelete);
+      })
+
+    return card;
+}
 
 // @todo: Функция удаления карточки
 
+function deleteCard(card) {
+    card.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(initialCard) {
+    const cardElement = createCard(initialCard, deleteCard);
+    placesList.append(cardElement);
+});
